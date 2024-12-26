0f6ec1465fad9ca21cac00936b82be499ea6276d
Lotrez <voltzlucien@gmail.com>
2024-12-26 13:32:44 +0100

    no cache test

--- app/lib/ai.ts
+++ app/lib/ai.ts
@@ -1,13 +1,8 @@
 import { createOpenAICompatible } from "@ai-sdk/openai-compatible";
-import type {
-	LanguageModelV1,
-	LanguageModelV1StreamPart,
-	StreamObjectResult,
-} from "ai";
+import type { StreamObjectResult } from "ai";
 import { experimental_wrapLanguageModel as wrapLanguageModel } from "ai";
 import { ollama } from "ollama-ai-provider";
 import type { AppLoadContext } from "react-router";
-import { createHash } from "./hash";
 
 const workersai = (API_KEY: string, ACCOUNT_ID: string) => {
 	console.log({
@@ -68,79 +63,79 @@ export const getModel = (context: AppLoadContext) => {
 					context.cloudflare.env.CLOUDFLARE_API_TOKEN,
 					context.cloudflare.env.CLOUDFLARE_ACCOUNT_ID,
 				)("@cf/meta/llama-3.3-70b-instruct-fp8-fast");
-	// return model;
+	return model;
 	const cache = context.cloudflare.env.KV_AI_CACHE;
 	return wrapLanguageModel({
 		model,
 		middleware: {
-			wrapGenerate: async ({ doGenerate, params }) => {
-				const cacheKey = createHash(JSON.stringify(params));
-				const cached = (await cache.get(cacheKey)) as Awaited<
-					ReturnType<LanguageModelV1["doGenerate"]>
-				> | null;
-				if (cached !== null) {
-					console.log("Cached!");
-					return {
-						...cached,
-						response: {
-							...cached.response,
-							timestamp: cached?.response?.timestamp
-								? new Date(cached?.response?.timestamp)
-								: undefined,
-						},
-					};
-				}
-				const result = await doGenerate();
-				cache.put(cacheKey, JSON.stringify(result));
-				return result;
-			},
-			wrapStream: async ({ doStream, params }) => {
-				const cacheKey = createHash(JSON.stringify(params));
-				console.log({ cacheKey });
-				// Check if the result is in the cache
-				const cached = await cache.get(cacheKey);
-				// If cached, return a simulated ReadableStream that yields the cached result
-				if (cached !== null) {
-					console.log("Cached!");
-					// Format the timestamps in the cached response
-					const formattedChunks = (
-						JSON.parse(cached) as LanguageModelV1StreamPart[]
-					).map((p) => {
-						if (p.type === "response-metadata" && p.timestamp) {
-							return { ...p, timestamp: new Date(p.timestamp) };
-						}
-						return p;
-					});
-					return {
-						stream: simulateReadableStream({
-							initialDelayInMs: 0,
-							chunkDelayInMs: 0,
-							chunks: formattedChunks,
-						}),
-						rawCall: { rawPrompt: null, rawSettings: {} },
-					};
-				}
-				// If not cached, proceed with streaming
-				const { stream, ...rest } = await doStream();
-				const fullResponse: LanguageModelV1StreamPart[] = [];
-				const transformStream = new TransformStream<
-					LanguageModelV1StreamPart,
-					LanguageModelV1StreamPart
-				>({
-					transform(chunk, controller) {
-						fullResponse.push(chunk);
-						controller.enqueue(chunk);
-					},
-					async flush() {
-						// Store the full response in the cache after streaming is complete
-						cache.put(cacheKey, JSON.stringify(fullResponse));
-					},
-				});
-				return {
-					stream: stream.pipeThrough(transformStream),
-					...rest,
-				};
-			},
+			// wrapGenerate: async ({ doGenerate, params }) => {
+			// 	const cacheKey = createHash(JSON.stringify(params));
+			// 	const cached = (await cache.get(cacheKey)) as Awaited<
+			// 		ReturnType<LanguageModelV1["doGenerate"]>
+			// 	> | null;
+			// 	if (cached !== null) {
+			// 		console.log("Cached!");
+			// 		return {
+			// 			...cached,
+			// 			response: {
+			// 				...cached.response,
+			// 				timestamp: cached?.response?.timestamp
+			// 					? new Date(cached?.response?.timestamp)
+			// 					: undefined,
+			// 			},
+			// 		};
+			// 	}
+			// 	const result = await doGenerate();
+			// 	cache.put(cacheKey, JSON.stringify(result));
+			// 	return result;
+			// },
+			// wrapStream: async ({ doStream, params }) => {
+			// 	const cacheKey = createHash(JSON.stringify(params));
+			// 	console.log({ cacheKey });
+			// 	// Check if the result is in the cache
+			// 	const cached = await cache.get(cacheKey);
+			// 	// If cached, return a simulated ReadableStream that yields the cached result
+			// 	if (cached !== null) {
+			// 		console.log("Cached!");
+			// 		// Format the timestamps in the cached response
+			// 		const formattedChunks = (
+			// 			JSON.parse(cached) as LanguageModelV1StreamPart[]
+			// 		).map((p) => {
+			// 			if (p.type === "response-metadata" && p.timestamp) {
+			// 				return { ...p, timestamp: new Date(p.timestamp) };
+			// 			}
+			// 			return p;
+			// 		});
+			// 		return {
+			// 			stream: simulateReadableStream({
+			// 				initialDelayInMs: 0,
+			// 				chunkDelayInMs: 0,
+			// 				chunks: formattedChunks,
+			// 			}),
+			// 			rawCall: { rawPrompt: null, rawSettings: {} },
+			// 		};
+			// 	}
+			// 	// If not cached, proceed with streaming
+			// 	const { stream, ...rest } = await doStream();
+			// 	const fullResponse: LanguageModelV1StreamPart[] = [];
+			// 	const transformStream = new TransformStream<
+			// 		LanguageModelV1StreamPart,
+			// 		LanguageModelV1StreamPart
+			// 	>({
+			// 		transform(chunk, controller) {
+			// 			fullResponse.push(chunk);
+			// 			controller.enqueue(chunk);
+			// 		},
+			// 		async flush() {
+			// 			// Store the full response in the cache after streaming is complete
+			// 			cache.put(cacheKey, JSON.stringify(fullResponse));
+			// 		},
+			// 	});
+			// 	return {
+			// 		stream: stream.pipeThrough(transformStream),
+			// 		...rest,
+			// 	};
+			// },
 		},
 	});
 };
